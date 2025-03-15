d28c0faa825ec2ced99b28620dc2eee2a941d449
azcpcf <99.a.cpcf.r.99@gmail.com>
2025-03-15 19:47:06 +0330

    add footer

--- src/ui/footer/index.tsx
+++ src/ui/footer/index.tsx
@@ -1,3 +1,15 @@
-export default function Footer() {
-  return <footer></footer>;
+import "./style.css";
+export default function Header() {
+  return (
+    <footer>
+      <span>
+        nirvana by <a href="https://github.com/AZCPCF" target="_blank">AZCPCF</a>
+      </span>
+      <h1>
+        <a href="https://github.com/AZCPCF/nirvana" target="_blank">
+          github
+        </a>
+      </h1>
+    </footer>
+  );
 }
