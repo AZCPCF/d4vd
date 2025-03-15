7cb90a492c35fab137fdb6e663ce651263abb3d1
azcpcf <99.a.cpcf.r.99@gmail.com>
2025-03-15 20:27:38 +0330

    change direction

--- src/ui/footer/index.tsx
+++ src/ui/footer/index.tsx
@@ -2,14 +2,14 @@ import "./style.css";
 export default function Header() {
   return (
     <footer>
-      <span>
-        nirvana by <a href="https://github.com/AZCPCF" target="_blank">AZCPCF</a>
-      </span>
       <h1>
         <a href="https://github.com/AZCPCF/nirvana" target="_blank">
           github
         </a>
       </h1>
+      <span>
+        nirvana by <a href="https://github.com/AZCPCF" target="_blank">AZCPCF</a>
+      </span>
     </footer>
   );
 }
