3c5c7235866f58764a221bebf113951d4f9bf990
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-03-17 08:18:27 +0330

    add text ellipsis

--- src/ui/components/AlbumCard/index.jsx
+++ src/ui/components/AlbumCard/index.jsx
@@ -5,19 +5,43 @@ import Typography from "@mui/material/Typography";
 
 export default function AlbumCard({ image, title, description }) {
   return (
-    <Card sx={{ width: "100%",height:'100%',backgroundColor:'primary.dark'}}>
-      <CardActionArea sx={{minHeight:'100%'}}>
-        <img src={`/images/${image}`}
-        alt={title}
-        width={'100%'}
-        height={'100%'}
-        style={{objectFit:'contain'}}
+    <Card
+      sx={{ width: "100%", height: "100%", backgroundColor: "primary.dark" }}
+    >
+      <CardActionArea sx={{ minHeight: "100%" }}>
+        <img
+          src={`/images/${image}`}
+          alt={title}
+          width={"100%"}
+          height={"100%"}
+          style={{ objectFit: "cover" }}
         />
         <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="div"
+            sx={{
+              width: "100%",
+              height: "40px",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+            }}
+          >
             {title}
           </Typography>
-          <Typography variant="body1" sx={{ color: "text.secondary" }}>
+          <Typography
+            variant="body1"
+            sx={{
+              color: "text.secondary",
+              width: "100%",
+              height: "40px",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+            }}
+          >
             {description}
           </Typography>
         </CardContent>
