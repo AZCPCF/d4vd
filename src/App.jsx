9f84f0cacc4dd65910c5cef8b9e9328e4ec4225c
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-03-17 08:08:37 +0330

    add songs

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import { MuiThemeProvider } from "./ui/theme"; // Fixed typo here
 import AlbumCardSkeleton from "./ui/components/AlbumSkeleton";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+  const [albums, setAlbums] = useState([]);
+  const [albumsLoading, setAlbumsLoading] = useState(false);
+  const [songs, setSongs] = useState([]);
+  const [songsLoading, setSongsLoading] = useState(false);
   const getData = async () => {
-    setLoading(true);
-    const res = await (await fetch(`${API.url}/albums`)).json();
-    setData(res);
-    setLoading(false);
+    setAlbumsLoading(true);
+    const albums = await (await fetch(`${API.url}/albums`)).json();
+    setAlbums(albums);
+    setAlbumsLoading(false);
+    setSongsLoading(true);
+    const songs = await (await fetch(`${API.url}/songs`)).json();
+    setSongs(songs);
+    setSongsLoading(false);
   };
 
   useEffect(() => {
@@ -44,7 +49,7 @@ const App = () => {
           </Typography>
           <Link href="/albums">more</Link>
         </Grid>
-        {loading
+        {albumsLoading
           ? Array.from({ length: skeleton.home.count }).map((_, index) => (
               <Grid
                 item
@@ -59,8 +64,62 @@ const App = () => {
                 <AlbumCardSkeleton />
               </Grid>
             ))
-          : data
-              ?.filter((item, index) => index <= 4)
+          : albums
+              ?.filter((_, index) => index <= 4)
+              ?.map((item, index) => (
+                <Grid
+                  item
+                  lg={12 / 5}
+                  md={12 / 4}
+                  sm={12 / 3}
+                  py={{ sm: 0, xs: 1 }}
+                  p={{ sm: 1 }}
+                  xs={12}
+                  key={index}
+                >
+                  <AlbumCard
+                    title={item?.title}
+                    description={item?.description}
+                    image={item?.image}
+                  />
+                </Grid>
+              ))}
+        <Grid
+          item
+          xs={12}
+          py={1}
+          sx={{ pr: { sm: 1 } }}
+          container
+          justifyContent={"space-evenly"}
+        >
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            Songs
+          </Typography>
+          <Link href="/albums">more</Link>
+        </Grid>
+        {songsLoading
+          ? Array.from({ length: skeleton.home.count }).map((_, index) => (
+              <Grid
+                item
+                lg={12 / 5}
+                md={12 / 4}
+                sm={12 / 3}
+                py={{ sm: 0, xs: 1 }}
+                p={{ sm: 1 }}
+                xs={12}
+                key={index}
+              >
+                <AlbumCardSkeleton />
+              </Grid>
+            ))
+          : songs
+              ?.filter((_, index) => index <= 4)
               ?.map((item, index) => (
                 <Grid
                   item
